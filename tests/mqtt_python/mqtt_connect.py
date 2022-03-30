495b5c05f14a41a15dd560584497203e7fe2a78b
Antoine <antoine.roth@etu.univ.smb.fr>
2022-03-30 16:10:12 +0200

    mqtt change

--- tests/mqtt_python/mqtt_connect.py
+++ tests/mqtt_python/mqtt_connect.py
@@ -1,17 +1,53 @@
 #install mqtt packages
-#npm i mqtt
+#pip install paho.mqtt
 
+from operator import indexOf
 import paho.mqtt.client as mqtt
-
+import json
 
 # Successful Connection Callback
 def on_connect(client, userdata, flags, rc):
     print('Connected with result code '+str(rc))
     client.subscribe('\showroom\PIR')
 
+# add new values to a list, delete old ones if too many values
+def append_limit(value, list):
+    newList = list
+    if(len(list)>=15):
+        del list[0]
+    newList.append(value)
+    return newList
+
+# moyennes des capteurs en paramètres 
+# Capteurs : PIR1, PIR2, PIR3, PIR4, PIR5
+def moys(list, capteurs = []):
+    sums = {}
+    for capteur in list:
+        (capt_test, v) = capteur[0]
+        if capt_test not in capteurs:
+            continue
+        sum = 0
+        name = ''
+        for k in capteur:
+            (capt, val) = k
+            name = capt
+            sum+=val
+        sums[name] = sum/len(capteur)
+    return sums
+
+capteurs_datas = []
 # Message delivery callback
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    message_json = json.loads(msg.payload.decode("utf-8"))
+    for capteur in message_json:
+        if(len(capteurs_datas)<5):
+            capteurs_datas.append([])
+        datas = capteurs_datas[message_json.index(capteur)]
+        for value in capteur.items():
+            append_limit(value, datas)
+    # print(msg.topic+" "+str(msg.payload))
+    print(moys(capteurs_datas, ['PIR1', 'PIR2']))
+    
 
 client = mqtt.Client()
 
